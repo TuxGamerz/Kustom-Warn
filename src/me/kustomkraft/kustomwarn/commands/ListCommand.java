c1a78698eaf0d8e1fa3de66cc611ef1d14d5e45e
gamerz <gamerzaction@gmail.com>
2013-05-06 22:54:13 +0100

    Shortened Messages in code form by creating prefix string

--- src/me/kustomkraft/kustomwarn/commands/ListCommand.java
+++ src/me/kustomkraft/kustomwarn/commands/ListCommand.java
@@ -7,11 +7,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
-import sun.util.resources.CalendarData_mk;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class ListCommand implements CommandExecutor {
 
@@ -25,50 +20,53 @@ public class ListCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
         ConsoleCommandSender consoleSender = sender.getServer().getConsoleSender();
         Player targetPlayer = sender.getServer().getPlayer(args[0]);
+        String prefix = ChatColor.GREEN + "[Kustom Warn]";
         if(commandLabel.equalsIgnoreCase("list")){
             if (!(sender instanceof Player)) {
                 if (args.length == 0) {
-                    consoleSender.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + "Not enough arguments!");
+                    consoleSender.sendMessage(prefix + ChatColor.RED + "Not enough arguments!");
                     return true;
                 }else if (args.length == 1) {
                     if (targetPlayer != null) {
                         if (!targetPlayer.hasPermission("kustomwarn.exempt")) {
                             if (this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) == 0) {
-                                consoleSender.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.GOLD + targetPlayer.getName() + " has not received any warnings!");
+                                consoleSender.sendMessage(prefix + ChatColor.GOLD + targetPlayer.getName() + " has not received any warnings!");
+                                return true;
+                            }else{
+                                consoleSender.sendMessage(prefix + ChatColor.RED + targetPlayer.getName() + " has been warned " + this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) + " time(s)");
                                 return true;
                             }
-                            consoleSender.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + targetPlayer.getName() + " has been warned " + this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) + " time(s)");
-                            return true;
                         }
-
                         return true;
                     }
-                    consoleSender.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + "Player not found!");
+                    consoleSender.sendMessage(prefix + ChatColor.RED + "Player not found!");
                     return true;
                 }
             } else {
                 Player player = (Player)sender;
-                if (player.hasPermission("kustomwarn.check")) {
-                    if (args.length == 0) {
-                        if (this.plugin.warnedPlayers.getWarnings(player.getName()) == 0) {
-                            player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.GOLD + "You have not received any warnings!");
-                            return true;
-                        }
-                        player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + "You have received " + this.plugin.warnedPlayers.getWarnings(player.getName()) + " warnings!");
-                    }else if (args.length == 1) {
-                        if (player.hasPermission("kustomwarn.check")) {
-                            if (targetPlayer != null) {
-                                if (this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) == 0) {
-                                    player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.GOLD + targetPlayer.getName() + " has not received any warnings!");
-                                    return true;
-                                }
-                                player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + targetPlayer.getName() + " has been warned " + this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) + " time(s)");
+                if (args.length == 0) {
+                    if (this.plugin.warnedPlayers.getWarnings(player.getName()) == 0) {
+                        player.sendMessage(prefix + ChatColor.GOLD + "You have not received any warnings!");
+                        return true;
+                    }else{
+                        player.sendMessage(prefix + ChatColor.RED + "You have received " + this.plugin.warnedPlayers.getWarnings(player.getName()) + " warnings!");
+                        return true;
+                    }
+                }else if (args.length == 1) {
+                    if (player.hasPermission("kustomwarn.check")) {
+                        if (targetPlayer != null) {
+                            if (this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) == 0) {
+                                player.sendMessage(prefix + ChatColor.GOLD + targetPlayer.getName() + " has not received any warnings!");
                                 return true;
                             }
-                            player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + "Player not found!");
+                            player.sendMessage(prefix + ChatColor.RED + targetPlayer.getName() + " has been warned " + this.plugin.warnedPlayers.getWarnings(targetPlayer.getName()) + " time(s)");
+                            return true;
+                        }else{
+                            player.sendMessage(prefix + ChatColor.RED + "Player not found!");
                             return true;
                         }
-                        player.sendMessage(ChatColor.GREEN + "[Kustom Warn]" + ChatColor.RED + "You don't have permission to perform this command!");
+                    }else{
+                        player.sendMessage(prefix + ChatColor.RED + "You don't have permission to perform this command!");
                         return true;
                     }
                 }
